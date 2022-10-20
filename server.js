37996445efa1887b784ba54337cf613884d8a954
Rana Dubauskas <ranadubauskas@Ranas-MacBook-Pro-4.local>
2022-10-20 12:45:20 -0500

    “message”

--- server.js
+++ server.js
@@ -12,27 +12,20 @@ app.use(bodyParser.json());
 const url = 'https://api.musixmatch.com/ws/1.1';
 const key = process.env.API_KEY;
 
- 
-/*
-var top10Songs = [];
-for(let j = 0; j<data.message.body.track_list.length; j++){
-  top10Songs.push(data.message.body.track_list[j].track.track_id);
-  console.log(top10songs);
-}
-res.send(top10Songs);
-*/
-
-//gets top 10 Taylor Swift Songs sorted by track ratings
-    axios.get(`${url}/track.search?q_artist=taylor%20swift&page_size=10&page=1&s_track_rating=desc&apikey=${key}`)
-  .then((response) => {
-    const data = response.data;
-    const i = getRandomInt(data.length);
-    res.send(data.message.body.track_list);
-  })
-  .catch((error) => {
-    console.log(error);
-  });
+//gets top 10 Taylor Swift Songs sorted by track rating
+app.get('/api/songs', function(req, res){
+  axios.get(`${url}/track.search?q_artist=taylor%20swift&page_size=10&page=1&s_track_rating=desc&apikey=${key}`)
+.then((response) => {
+  const data = response.data;
+  const i = getRandomInt(data.length);
+  res.send(data.message.body.track_list);
+})
+.catch((error) => {
+  console.log(error);
 });
+});
+ 
+
 //get lyrics for song based on id 
 app.get('/api/songs/lyrics/:id', function(req, res){
   let idParam = parseInt(req.params.id);
@@ -49,17 +42,6 @@ app.get('/api/songs/lyrics/:id', function(req, res){
 
 
 
-//taylor swift artist id: 259675
-//&apikey=f1865c1ca1fde95012cf743b577990fe
-/*
-axios.get('https://api.musixmatch.com/ws/1.1/matcher.lyrics.get?q_track=sexy%20and%20i%20know%20it&q_artist=lmfao&apikey=f1865c1ca1fde95012cf743b577990fe')
-  .then((response) => {
-    console.log(response.data);
-  })
-  .catch((error) => {
-    console.log(error);
-  });
-  */
 
   
   // Helper function - gets a random integer up to (but not including) the maximum
